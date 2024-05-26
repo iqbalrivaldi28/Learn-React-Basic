2476a87ca486c50396121729a1d523d15d7d3b01
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-26 13:57:19 +0700

    feat: add conditional rendering

--- src/MenuResto.jsx
+++ src/MenuResto.jsx
@@ -1,14 +1,15 @@
-import './MenuResto.css'
+import './MenuResto.css';
 
 const MenuResto = () => {
+  const isMakanan = true;
+
   return (
     <div className="menu-item ">
       <div>
         <b>Nama Menu:</b> <em>Nasi Bakar 🍙</em>
       </div>
-      <div>
-        <b>Harga:</b> <em>Rp. 20.000</em>
-      </div>
+      {/* {isMakanan === true ? <div>Tipe: Makanan</div> : <div>Tipe: Minuman</div>} */}
+      {isMakanan === true && <div>Tipe: Makanan</div>}
     </div>
   );
 };
