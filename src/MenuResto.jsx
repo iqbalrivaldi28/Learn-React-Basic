6d202dd260230358ce0b815e43c8c597ad75e73e
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-26 13:48:58 +0700

    feat: add styling component

--- src/MenuResto.jsx
+++ src/MenuResto.jsx
@@ -1,13 +1,15 @@
+import './MenuResto.css'
+
 const MenuResto = () => {
   return (
-    <>
+    <div className="menu-item ">
       <div>
         <b>Nama Menu:</b> <em>Nasi Bakar 🍙</em>
       </div>
       <div>
         <b>Harga:</b> <em>Rp. 20.000</em>
       </div>
-    </>
+    </div>
   );
 };
 
