9c59d9873c53ecbdc41d25d7161926acc001647c
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-26 14:17:12 +0700

    feat: add looping concept in MenuResto Component

--- src/MenuResto.jsx
+++ src/MenuResto.jsx
@@ -2,15 +2,49 @@ import './MenuResto.css';
 
 const MenuResto = () => {
   const isMakanan = true;
+  const menu = ['nasi bakar 🍘', 'mie ayam 🍜', 'pisang goreng 🍌', 'donat 🍩'];
+  const minuman = [
+    {
+      nama: 'susu 🥛',
+      harga: 'Rp. 5.000',
+    },
+    {
+      nama: 'teh 🍵',
+      harga: 'Rp. 3.000',
+    },
+    {
+      nama: 'kopi ☕️',
+      harga: 'Rp. 4.000',
+    },
+  ];
 
   return (
-    <div className="menu-item ">
-      <div>
-        <b>Nama Menu:</b> <em>Nasi Bakar 🍙</em>
-      </div>
-      {/* {isMakanan === true ? <div>Tipe: Makanan</div> : <div>Tipe: Minuman</div>} */}
-      {isMakanan === true && <div>Tipe: Makanan</div>}
-    </div>
+    <>
+      {menu.map((item, index) => {
+        return (
+          <div className="menu-item " key={index}>
+            <div>
+              <b>Nama Menu:</b> <em>{item}</em>
+            </div>
+            {isMakanan === true && <div>Tipe: Makanan</div>}
+          </div>
+        );
+      })}
+
+      {minuman.map((item, index) => {
+        return (
+          <div className="menu-item" key={index}>
+            <div>
+              <b>
+                Nama Minuman: <em>{item.nama}</em>
+              </b> 
+              <br />
+              <b>Harga:</b> <em>{item.harga}</em>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
